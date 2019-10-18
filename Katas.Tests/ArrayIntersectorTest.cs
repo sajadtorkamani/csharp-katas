5d74bcb90e2746e474f526231ed8baefe5f02419
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-10-18 19:07:00 +0100

    Solve kata

---
+++ Katas.Tests/ArrayIntersectorTest.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+
+namespace Katas.Tests
+{
+    [TestFixture]
+    public class ArrayIntersectorTest
+    {
+        [Test]
+        public void Intersect__returns_correct_result()
+        {
+            var a = new[] {"dog", "bar", "foo"};
+            var b = new[] {"foo", "bar", "cat"};
+            var c = new[] {"gin", "foo", "bar"};
+
+            Assert.AreEqual(new[] {"bar", "foo"}, ArrayIntersector.Intersect(a, b, c));
+        }
+
+        [Test]
+        public void Intersect__handles_empty_input()
+        {
+            Assert.AreEqual(new string[] { }, ArrayIntersector.Intersect(new string[][] { }));
+        }
+    }
+
+    /**
+     * Join all arrays
+     * Uniqify array ("dog", "bar", "foo", "cat", "gin")
+     * From unique array, select only items that occur in all arrays
+     */
+}
---
+++ Katas/ArrayIntersector.cs
@@ -0,0 +1,22 @@
+// https://www.codewars.com/kata/array-intersect-all/train/csharp
+
+using System.Linq;
+
+namespace Katas
+{
+    public static class ArrayIntersector
+    {
+        public static string[] Intersect(params string[][] arrays)
+        {
+            if (arrays.Length == 0)
+            {
+                return new string[] { };
+            }
+
+            return arrays
+                .Aggregate((a, b) => a.Intersect(b).ToArray())
+                .Where(item => arrays.All(arr => arr.Contains(item)))
+                .ToArray();
+        }
+    }
+}
