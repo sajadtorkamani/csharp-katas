e7c793511fa725051b0cccd5dfd86ec5b191412b
Sajad Torkamani <sajadtorkamani1@gmail.com>
2020-09-17 13:51:14 +0100

    Solve kata

---
+++ Katas.Tests/StringCalculatorTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Katas.Tests
+{
+    [TestFixture]
+    class StringCalculatorTest
+    {
+        [Test]
+        public void AddStrings_ReturnsSumOfNumsInString()
+        {
+            Assert.AreEqual(5, StringCalculator.AddStrings("2, 3"));
+            Assert.AreEqual(60, StringCalculator.AddStrings("20, 20, 20"));
+        }
+    }
+}
---
+++ Katas/StringCalculator.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Linq;
+
+namespace Katas
+{
+    public class StringCalculator
+    {
+        public static int AddStrings(string expression)
+        {
+            var sum = expression.Split(", ").Select(Int32.Parse).Sum();
+
+            return sum;
+        }
+    }
+}
