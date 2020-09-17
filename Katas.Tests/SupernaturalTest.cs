999c93bd3948a8a0b89bf46fa34513d423a81705
Sajad Torkamani <sajadtorkamani1@gmail.com>
2020-09-17 15:37:30 +0100

    Solve kata

---
+++ Katas.Tests/SupernaturalTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Katas.Tests
+{
+    [TestFixture]
+    class SupernaturalTest
+    {
+        [Test]
+        public void Bob_ReturnsCorrectKillingTechniqueBaseOnEnemyType()
+        {
+            Assert.AreEqual("Behead it with a machete, idjits!", Supernatural.Bob("vampire"));
+            Assert.AreEqual("It depends on which one it is, idjits!", Supernatural.Bob("pagan god"));
+        }
+
+        [Test]
+        public void Bob_HandlesUnknownEnemyTypes()
+        {
+            Assert.AreEqual("I have friggin no idea yet, idjits!", Supernatural.Bob("werepuppy"));
+        }
+    }
+}
