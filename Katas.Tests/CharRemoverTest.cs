7e5b470c630fa659bf37827a54da822c907903d3
Sajad Torkamani <sajadtorkamani1@gmail.com>
2021-01-07 07:10:42 +0000

    Remove comments

--- Katas.Tests/CharRemoverTest.cs
+++ Katas.Tests/CharRemoverTest.cs
@@ -19,20 +19,6 @@ namespace Kata.Tests
                 "hello world",
                 CharRemover.Remove("hello world", new Dictionary<char, int> {{'x', 5}, {'i', 2}})
             );
-
-//            Assert.AreEqual("pples d bnns",
-//                CharRemover.Remove("apples and bananas", new Dictionary<char, int> {{'a', 50}, {'n', 1}}));
-//            Assert.AreEqual("", CharRemover.Remove("a", new Dictionary<char, int> {{'a', 1}, {'n', 1}}));
-//            Assert.AreEqual("",
-//                CharRemover.Remove("codewars",
-//                    new Dictionary<char, int>
-//                        {{'c', 5}, {'o', 1}, {'d', 1}, {'e', 1}, {'w', 1}, {'z', 1}, {'a', 1}, {'r', 1}, {'s', 1}}));
         }
     }
-
-    /**
-     * Iterate over each item in dictionary
-     *     Remove n occurrences of char from string
-     * Return modified string
-     */
 }
